bc74f33be2e7d025b3d05ee235270ac979ca4e78
Carlos Pajares <c.pajares@hotmail.com>
2021-11-05 22:05:08 +0100

    created put /update way route to updateRobotById

--- server/routes/robotRoutes.js
+++ server/routes/robotRoutes.js
@@ -3,6 +3,7 @@ const {
   getRobots,
   getRobotbyId,
   createRobot,
+  updateRobotById,
 } = require("../controller/robotController");
 
 const router = express.Router();
@@ -12,5 +13,7 @@ router.get("/", getRobots);
 router.get("/:idRobot", getRobotbyId);
 
 router.post("/create", createRobot);
+
+router.put("/update", updateRobotById);
 
 module.exports = router;
