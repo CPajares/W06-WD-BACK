7dd8ca51e7529804aa194c1b77f0ccdbb8306437
Carlos Pajares <c.pajares@hotmail.com>
2021-11-05 21:11:57 +0100

    Added createRobot function  at robot Controller and called in robotRoutes

--- server/routes/robotRoutes.js
+++ server/routes/robotRoutes.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { getRobots, getRobotbyId } = require("../controller/robotController");
+const {
+  getRobots,
+  getRobotbyId,
+  createRobot,
+} = require("../controller/robotController");
 
 const router = express.Router();
 
 router.get("/", getRobots);
 
 router.get("/:idRobot", getRobotbyId);
+
+router.post("/create", createRobot);
 
 module.exports = router;
