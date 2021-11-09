967cf60e56027a1f763ac3d2cc118efa2a39cd87
Carlos Pajares <c.pajares@hotmail.com>
2021-11-09 20:14:02 +0100

    Modificated DB structure for test

--- database/index.js
+++ database/index.js
@@ -3,16 +3,22 @@ const chalk = require("chalk");
 const debug = require("debug")("robots:DB");
 const mongoose = require("mongoose");
 
-const initiateDB = () => {
-  mongoose.connect(process.env.MONGO_DB, (error) => {
-    if (error) {
-      debug(chalk.red("No se ha podido iniciar la base de datos."));
-      debug(chalk.red(error.message));
-      return;
-    }
+const initiateDB = (conectingString) =>
+  new Promise((resolve, reject) => {
+    mongoose.connect(conectingString, (error) => {
+      if (error) {
+        debug(chalk.red("No se ha podido iniciar la base de datos."));
+        debug(chalk.red(error.message));
+        reject();
+        return;
+      }
 
-    debug(chalk.green("in the funcking DB"));
+      debug(chalk.green("in the funcking DB"));
+      resolve();
+    });
+    mongoose.connection.on("close", () => {
+      debug(chalk.red("Desconectado de la DB"));
+    });
   });
-};
 
 module.exports = initiateDB;
